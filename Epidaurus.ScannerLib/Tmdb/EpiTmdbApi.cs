7ecdd5e27dd3fedc8cf698e8577de2040145f414
Bjørn Moe <bem@farlig.org>
2011-11-22 21:07:32 +0100

    Beginning tmdb impl

---
+++ Epidaurus.ScannerLib/Tmdb/EpiTmdbApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Epidaurus.ScannerLib.Tmdb
+{
+    class EpiTmdbApi
+    {
+        public EpiTmdbApi()
+        {
+            var api = new TheMovieDb.TmdbApi(ApiKey);
+
+            var titles = api.MovieSearch("the matrix");
+
+            foreach (var title in titles)
+            {
+
+            }
+        }
+
+        protected string ApiKey
+        {
+            get { return ConfigurationManager.AppSettings["tmdbApiKey"].Trim(); }
+        }
+    }
+}
