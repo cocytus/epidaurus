01e1685eee1c52a3142d327ee2cd0e6226b8ef36
Bjørn Moe <bem@farlig.org>
2011-12-10 16:28:17 +0100

    Added simple mime type resolver

---
+++ Epidaurus.Domain/Mime/IMimeTypeResolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epidaurus.Domain.Mime
+{
+    public interface IMimeTypeResolver
+    {
+        string Resolve(string fileName);
+        string Resolve(string fileName, string defaultMimeType);
+    }
+}
---
+++ Epidaurus.Domain/Mime/StaticMimeTypeResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epidaurus.Domain.Mime
+{
+    public class StaticMimeTypeResolver : IMimeTypeResolver
+    {
+        public string Resolve(string fileName)
+        {
+            var ret = Resolve(fileName, null);
+            if (ret == null)
+                throw new InvalidOperationException("Unknown file type");
+            return ret;
+        }
+
+        public string Resolve(string fileName, string defaultMimeType)
+        {
+            var ext = System.IO.Path.GetExtension(fileName);
+            if (string.IsNullOrEmpty(ext))
+                throw new ArgumentException("filenName", "Not a file name");
+            string ret;
+            if (!_mimeMap.TryGetValue(ext.ToLowerInvariant(), out ret))
+                return defaultMimeType;
+            return ret;
+        }
+
+        private static Dictionary<string, string> _mimeMap = new Dictionary<string,string>()
+        {
+            { ".avi", "video/avi" },
+            { ".mkv", "video/x-matroska"},
+            { ".wmv", "video/x-ms-wmv"},
+            { ".mp4", "video/mp4 "}
+        };
+    }
+}
